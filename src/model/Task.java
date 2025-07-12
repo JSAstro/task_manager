8194e0ee4e4dade415efbb567aab4d5d1df3b090
ASTRO <jordansnrz@gmail.com>
2025-07-11 21:52:29 -0300

    repensee 0.02

--- src/model/Task.java
+++ src/model/Task.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
+import java.util.Date;
+import java.time.LocalDateTime;
 
-/**
- *
- * @author Jordan
- */
 public class Task {
-    
+	private String tittle;
+	private String caption;
+	private LocalDateTime created_time;
+        private char prioridad;
+	private String body;
+	private Usuario user;
+	
+        public Task(){
+            
+        }
+        public void setCreated_time(){
+            this.created_time = LocalDateTime.now();
+        }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public LocalDateTime getCreated_time() {
+        return created_time;
+    }
+
+    public char getPrioridad() {
+        return prioridad;
+    }
+
+    public String getBody() {
+        return body;
+    }
+        
+        
 }
+
--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,13 +1,73 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author Jordan
- */
 public class Usuario {
-    
+	private String username;
+	private String first_name;
+	private String middle_name;
+	private String surname;
+	private int age;
+	private boolean active;
+	
+	public Usuario(String username, String first_name, String middle_name,String surname, int age) {
+		this.username = username;
+		this.first_name = first_name;
+		this.middle_name = middle_name;
+		this.surname = surname;
+		this.age = age;
+		this.active = true;
+	}
+	public void modificarUsuario(String username){
+		this.username = username;
+	}
+	
+	public boolean inactivo() {
+		this.active=false;
+		return active;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getMiddle_name() {
+		return middle_name;
+	}
+
+	public void setMiddle_name(String middle_name) {
+		this.middle_name = middle_name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
 }
+
