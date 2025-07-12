2539273df954b20bc68d16f817c848bb4cf95755
ASTRO <jordansnrz@gmail.com>
2025-07-11 21:54:58 -0300

    repensee 0.02 new

--- src/model/Task.java
+++ src/model/Task.java
@@ -1,42 +1,7 @@
 package model;
-import java.util.Date;
-import java.time.LocalDateTime;
 
-public class Task {
-	private String tittle;
-	private String caption;
-	private LocalDateTime created_time;
-        private char prioridad;
-	private String body;
-	private Usuario user;
-	
-        public Task(){
-            
-        }
-        public void setCreated_time(){
-            this.created_time = LocalDateTime.now();
-        }
-
-    public String getTittle() {
-        return tittle;
-    }
-
-    public String getCaption() {
-        return caption;
-    }
-
-    public LocalDateTime getCreated_time() {
-        return created_time;
-    }
-
-    public char getPrioridad() {
-        return prioridad;
-    }
-
-    public String getBody() {
-        return body;
-    }
-        
-        
+public interface Task {
+	void save();
+        void edit();
+        void delete();             
 }
-
--- src/model/TypeTask.java
+++ src/model/TypeTask.java
@@ -1,5 +1,34 @@
 package model;
 
-public class TypeTask {
+public class TypeTask implements Task{
+    private String name;
+    private String body;
+    private Usuario user;
     
+    public TypeTask(){
+        
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public void setUser(Usuario user) {
+        this.user = user;
+    }
+    
+    public TypeTask save(TypeTask task){
+        // Aca code para guardar en la BD
+        return this;
+    }
+    public void edit(String name, String body, Usuario user){
+        setName(name);
+        setBody(body);
+        setUser(user);
+    }
+    public void delete();
 }
