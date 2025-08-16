e993abb2a649d681b7032fac34bd7c81bfea0dcf
AstroJs <qj.jordan@gmail.com>
2025-08-15 22:03:42 -0300

    Recambio de patron de diseño

--- src/Control/Controller.java
+++
@@ -1,8 +0,0 @@
-
-package Control;
-import model.Task;
-import model.TypeTask;
-
-public class Controller {
-    private Task newTask = new TypeTask();
-}
--- src/model/Task.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public interface Task {
-	TypeTask save();
-        void edit(String name, String body, Usuario user);
-        void delete();             
-}
